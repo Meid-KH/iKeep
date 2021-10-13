a4e7b1ddee3fa0fc546e8c221246c3e00e36ee6c
Muhamed <maeid.kh@gmail.com>
2021-10-13 01:50:11 +0100

    - update seed

--- prisma/seed.js
+++ prisma/seed.js
@@ -2,7 +2,7 @@ const faker = require("faker");
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-const data = Array.from({ length: 100 }).map(() => ({
+const data = Array.from({ length: 10 }).map(() => ({
 	title: faker.lorem.sentence(),
 	content: faker.lorem.sentence(),
 }));
@@ -11,12 +11,13 @@ async function main() {
 	await prisma.note.createMany({
 		data: data,
 	});
+		console.log(" seeeeeeeeeeeeeding !!!!");
 }
 
 main()
 	.catch((e) => {
-		console.log(e);
-		process.exit(1);
+		console.log("Wa seeeeeeeeeeeeed");
+		process.exit(0);
 	})
 	.finally(() => {
 		prisma.$disconnect();
